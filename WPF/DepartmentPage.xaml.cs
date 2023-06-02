16228d4425c9c059139e6d39cc63007edc86c7d3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-02 14:14:58 -0300

    add and design pages

--- WPF/DepartmentPage.xaml.cs
+++ WPF/DepartmentPage.xaml.cs
@@ -3,18 +3,18 @@
 namespace WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DepartmentPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DepartmentPage : Window
     {
-        public MainWindow()
+        public DepartmentPage()
         {
             InitializeComponent();
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            this.Close();
         }
     }
 }
