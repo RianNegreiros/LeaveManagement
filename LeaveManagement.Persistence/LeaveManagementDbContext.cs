bdd883c12d1c5294341e0311a4ce896460976385
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-07 12:12:30 -0300

    add persistence layer

--- LeaveManagement.Persistence/LeaveManagementDbContext.cs
+++ LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -1,11 +1,12 @@
+using LeaveManagement.Domain;
+using LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using LeaveManagement.Core.Domain;
-
-namespace LeaveManagement.Infrastructure
+namespace LeaveManagement.Persistence
 {
   public class LeaveManagementDbContext : DbContext
   {
