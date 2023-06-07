5647a8bec55c45cefb4cf36d63bdfa2c4682a107
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-07 12:17:10 -0300

    add email sender

---
+++ Core/LeaveManagement.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,13 @@
+using LeaveManagement.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.Contracts.Infrastructure
+{
+  public interface IEmailSender
+  {
+    Task<bool> SendEmail(Email email);
+  }
+}
