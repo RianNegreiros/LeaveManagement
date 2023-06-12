e45bb1f90897a6d7c65aebf3cc2f85306288416f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-12 16:11:20 -0300

    configure

--- LeaveManagement.API/Program.cs
+++ LeaveManagement.API/Program.cs
@@ -1,3 +1,6 @@
+using LeaveManagement.Application;
+using LeaveManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +10,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureInfrastructureServices(builder.Configuration);
+builder.Services.ConfigurePersistenceServices(builder.Configuration);
+
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new() { Title = "LeaveManagement.API", Version = "v1" });
+});
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("CorsPolicy", builder =>
+  {
+    builder.AllowAnyOrigin()
+          .AllowAnyHeader()
+          .AllowAnyMethod();
+  });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
