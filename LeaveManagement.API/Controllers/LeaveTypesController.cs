bb5351225b9ee29f490edb82ad1b81001b7a8c98
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-14 15:37:34 -0300

    add auth to api

--- LeaveManagement.API/Controllers/LeaveTypesController.cs
+++ LeaveManagement.API/Controllers/LeaveTypesController.cs
@@ -2,12 +2,14 @@ using LeaveManagement.Application.DTOs.LeaveType;
 using LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
 using LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LeaveManagement.API.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
+  [Authorize(Roles = "Administrator")]
   public class LeaveTypesController : ControllerBase
   {
     private readonly IMediator _mediator;
